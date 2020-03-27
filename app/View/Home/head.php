bd7c03f607c786028b448141089180ff3d2032b7
Misael Guilhardes <misaelg.freitas2000@gmail.com>
2020-03-26 22:53:43 -0300

    Revert "Commiting changes on frontend an new backend"
    This reverts commit 1931c66dd136ae0daae3e7311d7df414297740a4.

--- app/View/Home/head.php
+++
@@ -1,1 +0,0 @@
-<link rel="stylesheet" type="text/css" href="<?=DIRPAGE?>app/View/Home/styles.css">
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once './vendor/autoload.php';
-include_once './config/config.php';
-
-error_reporting(E_ALL); ini_set('display_errors', 1); 
-use App\Cores\Dispatch;
-$d = new Dispatch();
-
-?>
